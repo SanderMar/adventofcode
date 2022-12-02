ba9e62292de13d103438a829312fc0a806902276
SanderMar <sander.marinus@student.uantwerpen.be>
2022-12-02 11:01:06 +0100

    Solve 2022 day 2 part 2

--- aoc2022/src/day02.rs
+++ aoc2022/src/day02.rs
@@ -20,6 +20,26 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
+#[aoc(day2, part2)]
+fn part2(input: &str) -> u32 {
+    input
+        .split("\n")
+        .map(|l| {
+            let game_state: u8 = l.chars().nth(2).unwrap() as u8 - 'X' as u8;
+            let opponent: u8 = l.chars().nth(0).unwrap() as u8 - 'A' as u8;
+            let own_play: i32;
+            if game_state == 2 {
+                own_play = (opponent as i32 + 1).rem_euclid(3) + 1;
+            } else if game_state == 0 {
+                own_play = (opponent as i32 - 1).rem_euclid(3) + 1;
+            } else {
+                own_play = opponent as i32 + 1;
+            }
+            game_state as u32 * 3 + own_play as u32
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod part1_tests {
     use super::*;
@@ -30,4 +50,14 @@ mod part1_tests {
         assert_eq!(part1(&input), 15)
     }
 }
+
 #[cfg(test)]
+mod part2_tests {
+    use super::*;
+
+    #[test]
+    fn input_website() {
+        let input = "A Y\nB X\nC Z";
+        assert_eq!(part2(&input), 12)
+    }
+}
