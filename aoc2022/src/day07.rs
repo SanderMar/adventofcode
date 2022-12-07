f8cc36335868c5d23043e985b1d38e4ee6e04355
SanderMar <sander.marinus@student.uantwerpen.be>
2022-12-07 12:33:22 +0100

    Fix 2022 day 7 generator for directory levels

--- aoc2022/src/day07.rs
+++ aoc2022/src/day07.rs
@@ -4,10 +4,18 @@ use std::collections::HashMap;
 
 type Dir = (Vec<String>, u32);
 
+/// Construct directory name from all directories on stack
+fn name_from_stack(stack: &Vec<&str>) -> String {
+    stack
+        .iter()
+        .map(|s| s.to_owned().to_owned() + "/")
+        .collect()
+}
+
 #[aoc_generator(day7)]
-pub fn gen_day7(term_lines: &str) -> HashMap<&str, Dir> {
+pub fn gen_day7(term_lines: &str) -> HashMap<String, Dir> {
     let mut dir_stack: Vec<&str> = Vec::new();
-    let mut dir_map: HashMap<&str, Dir> = HashMap::new();
+    let mut dir_map: HashMap<String, Dir> = HashMap::new();
 
     term_lines.split("\n").for_each(|line| {
         let l_output: Vec<&str> = line.split(" ").collect();
@@ -18,17 +26,17 @@ pub fn gen_day7(term_lines: &str) -> HashMap<&str, Dir> {
                 dir_stack.pop();
             } else if l_output[1] == "cd" {
                 dir_stack.push(l_output[2]);
-                dir_map.insert(l_output[2], (Vec::new(), 0));
+                dir_map.insert(name_from_stack(&dir_stack), (Vec::new(), 0));
             }
         } else {
             if l_output[0] == "dir" {
                 dir_map
-                    .entry(dir_stack.last().unwrap())
-                    .and_modify(|(d, _)| d.push(l_output[1].to_owned()));
+                    .entry(name_from_stack(&dir_stack))
+                    .and_modify(|(d, _)| d.push(name_from_stack(&dir_stack) + l_output[1] + "/"));
             } else {
                 let size: u32 = l_output[0].parse::<u32>().unwrap();
                 dir_map
-                    .entry(dir_stack.last().unwrap())
+                    .entry(name_from_stack(&dir_stack))
                     .and_modify(|(_, s)| *s += size);
             }
         }
@@ -66,11 +74,14 @@ mod generator_test {
                      8033020 d.log\n\
                      5626152 d.ext\n\
                      7214296 k";
-        let mut expected: HashMap<&str, Dir> = HashMap::new();
-        expected.insert("/", (vec!["a".to_owned(), "d".to_owned()], 23352670));
-        expected.insert("a", (vec!["e".to_owned()], 94269));
-        expected.insert("e", (Vec::new(), 584));
-        expected.insert("d", (Vec::new(), 24933642));
+        let mut expected: HashMap<String, Dir> = HashMap::new();
+        expected.insert(
+            "//".to_owned(),
+            (vec!["//a/".to_owned(), "//d/".to_owned()], 23352670),
+        );
+        expected.insert("//a/".to_owned(), (vec!["//a/e/".to_owned()], 94269));
+        expected.insert("//a/e/".to_owned(), (Vec::new(), 584));
+        expected.insert("//d/".to_owned(), (Vec::new(), 24933642));
         assert_eq!(gen_day7(input), expected);
     }
 }
