c702ed1f8e7b895b6ddd8970938db0853c9d681a
SanderMar <sander.marinus@student.uantwerpen.be>
2023-01-16 18:47:27 +0100

    Start 2022 day 9, no working solution

--- aoc2022/src/day09.rs
+++ aoc2022/src/day09.rs
@@ -71,3 +71,107 @@ pub fn part1(moves: &[Move]) -> usize {
 
     visited.into_iter().count()
 }
+
+fn perform_move(rope: &mut [Pos], dir: Pos, i: usize) {
+    if dir == Pos(0, 0) || i == rope.len() {
+        return
+    }
+    if i == 0 {
+        rope[i] += dir;
+        return perform_move(rope, dir, i+1)
+    }
+
+    let pred_pos = rope[i - 1];
+    let mut final_dir = Pos(0, 0);
+    if rope[i].dist_to(&pred_pos) > 1 {
+        let mut tmp_pos = rope[i];
+        tmp_pos += dir;
+        final_dir = dir;
+
+        let check: bool = dir.0.abs() > 0 && dir.1.abs() > 0;
+        let diag: bool = tmp_pos.0 - pred_pos.0 != 0 && tmp_pos.1 - pred_pos.1 != 0 && !check;
+        if diag && dir.0 != 0 {
+            final_dir.1 += pred_pos.1 - tmp_pos.1;
+        } else if diag && dir.1 != 0 {
+            final_dir.0 += pred_pos.0 - tmp_pos.0;
+        }
+    }
+    rope[i] += final_dir;
+    return perform_move(rope, final_dir, i+1)
+}
+
+#[aoc(day9, part2)]
+pub fn part2(moves: &[Move]) -> usize {
+    // 1 == head, 2 == tail
+    let mut rope: Vec<Pos> = vec![Pos(0, 0); 10];
+    let mut visited: HashSet<Pos> = HashSet::new();
+    visited.insert(Pos(0, 0));
+
+    moves.into_iter().for_each(|&(dir, c)| {
+        for _ in 0..c {
+            perform_move(&mut rope, dir, 0);
+            visited.insert(*rope.last().unwrap());
+            // show_rope(&rope);
+        }
+        println!("{:?}", rope);
+    });
+
+    visited.into_iter().count()
+}
+
+pub fn show_rope(rope: &[Pos]) {
+    let max_x: usize = rope.iter().map(|&x| x.0.abs()).max().unwrap() as usize;
+    let max_y: usize = rope.iter().map(|&y| y.1.abs()).max().unwrap() as usize;
+    let mut grid = vec![vec!["."; max_y + 1]; max_x + 1];
+    rope.iter().for_each(|pos| grid[pos.0.abs() as usize][pos.1.abs() as usize] = "o");
+    grid.iter().rev().for_each(|row| println!("{:?}", row));
+}
+
+#[cfg(test)]
+mod generator_tests {
+    use super::*;
+
+    #[test]
+    fn input_website() {
+        let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
+        let expected: Vec<Move> = vec![
+            (Pos(0, 1), 4),
+            (Pos(-1, 0), 4),
+            (Pos(0, -1), 3),
+            (Pos(1, 0), 1),
+            (Pos(0, 1), 4),
+            (Pos(1, 0), 1),
+            (Pos(0, -1), 5),
+            (Pos(0, 1), 2),
+        ];
+        assert_eq!(gen_day9(input), expected)
+    }
+}
+
+#[cfg(test)]
+mod part1_tests {
+    use super::*;
+
+    #[test]
+    fn input_website() {
+        let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
+        assert_eq!(part1(&gen_day9(input)), 13)
+    }
+}
+
+#[cfg(test)]
+mod part2_tests {
+    use super::*;
+
+    #[test]
+    fn input_website() {
+        let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
+        assert_eq!(part2(&gen_day9(input)), 1)
+    }
+
+    #[test]
+    fn large_input() {
+        let input = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20";
+        assert_eq!(part2(&gen_day9(input)), 36)
+    }
+}
